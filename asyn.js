410252ca05a5482dd79476c9c322f836dd51765f
Eric Laursen <ericjlaursen@gmail.com>
2020-11-01 14:43:56 -0500

    nested try catch example

--- asyn.js
+++ asyn.js
@@ -1,19 +1,30 @@
-async function water(){
-  try{
-    await waitFn();
-  }catch(e){
-    console.log('error')
-    console.log(e);
+// async function water(){
+//   try{
+//     await waitFn();
+//   }catch(e){
+//     console.log('error')
+//     console.log(e);
+//   }
+// }
+
+// const waitFn = () => {
+//   return new Promise((res,rej) => {
+//     setTimeout(() => {
+//       console.log('done with delay')
+//       res()
+//     }, 2000)
+//   })
+// }
+
+// water()
+
+try {
+  try {
+    throw new Error('oops');
+  } catch (ex) {
+    // console.error('inner', ex.message);
+    throw ex;
   }
+} catch (ex) {
+  console.error('outer', ex.message);
 }
-
-const waitFn = () => {
-  return new Promise((res,rej) => {
-    setTimeout(() => {
-      console.log('done with delay')
-      res()
-    }, 2000)
-  })
-}
-
-water()
